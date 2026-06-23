1f0c51ebaa5d734a4b7f13ee2cdd904107b016b5
DURGA PRASAD PAPUGANI <papuganidurgaprasad@gmail.com>
2026-06-23 09:57:25 +0530

    Update DBConnector.java

--- StudentManagementSystem/DBConnector.java
+++ StudentManagementSystem/DBConnector.java
@@ -2,7 +2,7 @@ import java.sql.*;
 public class DBConnector {
 	private static String url="jdbc:mysql://localhost:3306/Student" ;
 	private static String user_name="root";
-	private static String pwd="Anudeep@99";
+	private static String pwd="root";
 	public static Connection getconnection() {
 		try {
 			return(DriverManager.getConnection(url,user_name,pwd));
